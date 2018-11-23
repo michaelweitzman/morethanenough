5591658fa1d0e4b9010abbd90967ed8d5e2b7c2f
Michael Weitzman <weitzman.michaelanthony@gmail.com>
2018-11-22 22:29:10 -0600

    simple map setup

---
+++ static/js/mapdata.js
@@ -0,0 +1,263 @@
+var simplemaps_statemap_mapdata={
+
+    main_settings: {
+
+        //General settings
+        width: "responsive",
+        border_color: "white",
+        popups: "detect",
+
+        //State defaults
+        state_description: "",
+        state_color: "#34393d",
+        state_hover_color: "#37abcb",
+        border_size: "1.5",
+
+        //Popup settings
+        popup_color: "white",
+        popup_opacity: 1,
+        popup_shadow: 3,
+        popup_corners: 1,
+        popup_font: "13px/1.5 Verdana, Arial, Helvetica, sans-serif",
+
+        //Advanced settings
+        div: "map",
+        fade_time: 0.1,
+
+        },
+
+        //State specific
+        state_specific: {
+        "40001": {
+          name: "Adair"
+        },
+        "40003": {
+          name: "Alfalfa"
+        },
+        "40005": {
+          name: "Atoka"
+        },
+        "40007": {
+          name: "Beaver"
+        },
+        "40009": {
+          name: "Beckham"
+        },
+        "40011": {
+          name: "Blaine"
+        },
+        "40013": {
+          name: "Bryan"
+        },
+        "40015": {
+          name: "Caddo"
+        },
+        "40017": {
+          name: "Canadian"
+        },
+        "40019": {
+          name: "Carter"
+        },
+        "40021": {
+          name: "Cherokee"
+        },
+        "40023": {
+          name: "Choctaw"
+        },
+        "40025": {
+          name: "Cimarron"
+        },
+        "40027": {
+          name: "Cleveland"
+        },
+        "40029": {
+          name: "Coal"
+        },
+        "40031": {
+          name: "Comanche"
+        },
+        "40033": {
+          name: "Cotton"
+        },
+        "40035": {
+          name: "Craig"
+        },
+        "40037": {
+          name: "Creek"
+        },
+        "40039": {
+          name: "Custer"
+        },
+        "40041": {
+          name: "Delaware"
+        },
+        "40043": {
+          name: "Dewey"
+        },
+        "40045": {
+          name: "Ellis"
+        },
+        "40047": {
+          name: "Garfield"
+        },
+        "40049": {
+          name: "Garvin"
+        },
+        "40051": {
+          name: "Grady"
+        },
+        "40053": {
+          name: "Grant"
+        },
+        "40055": {
+          name: "Greer"
+        },
+        "40057": {
+          name: "Harmon"
+        },
+        "40059": {
+          name: "Harper"
+        },
+        "40061": {
+          name: "Haskell"
+        },
+        "40063": {
+          name: "Hughes"
+        },
+        "40065": {
+          name: "Jackson"
+        },
+        "40067": {
+          name: "Jefferson"
+        },
+        "40069": {
+          name: "Johnston"
+        },
+        "40071": {
+          name: "Kay"
+        },
+        "40073": {
+          name: "Kingfisher"
+        },
+        "40075": {
+          name: "Kiowa"
+        },
+        "40077": {
+          name: "Latimer"
+        },
+        "40079": {
+          name: "LeFlore"
+        },
+        "40081": {
+          name: "Lincoln"
+        },
+        "40083": {
+          name: "Logan"
+        },
+        "40085": {
+          name: "Love"
+        },
+        "40087": {
+          name: "McClain"
+        },
+        "40089": {
+          name: "McCurtain"
+        },
+        "40091": {
+          name: "McIntosh"
+        },
+        "40093": {
+          name: "Major"
+        },
+        "40095": {
+          name: "Marshall"
+        },
+        "40097": {
+          name: "Mayes"
+        },
+        "40099": {
+          name: "Murray"
+        },
+        "40101": {
+          name: "Muskogee"
+        },
+        "40103": {
+          name: "Noble"
+        },
+        "40105": {
+          name: "Nowata"
+        },
+        "40107": {
+          name: "Okfuskee"
+        },
+        "40109": {
+          name: "Oklahoma"
+        },
+        "40111": {
+          name: "Okmulgee"
+        },
+        "40113": {
+          name: "Osage"
+        },
+        "40115": {
+          name: "Ottawa"
+        },
+        "40117": {
+          name: "Pawnee"
+        },
+        "40119": {
+          name: "Payne"
+        },
+        "40121": {
+          name: "Pittsburg"
+        },
+        "40123": {
+          name: "Pontotoc"
+        },
+        "40125": {
+          name: "Pottawatomie"
+        },
+        "40127": {
+          name: "Pushmataha"
+        },
+        "40129": {
+          name: "Roger Mills"
+        },
+        "40131": {
+          name: "Rogers"
+        },
+        "40133": {
+          name: "Seminole"
+        },
+        "40135": {
+          name: "Sequoyah"
+        },
+        "40137": {
+          name: "Stephens"
+        },
+        "40139": {
+          name: "Texas"
+        },
+        "40141": {
+          name: "Tillman"
+        },
+        "40143": {
+          name: "Tulsa"
+        },
+        "40145": {
+          name: "Wagoner"
+        },
+        "40147": {
+          name: "Washington"
+        },
+        "40149": {
+          name: "Washita"
+        },
+        "40151": {
+          name: "Woods"
+        },
+        "40153": {
+          name: "Woodward"
+        }
+    }
+};
